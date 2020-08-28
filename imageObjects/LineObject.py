2b404b7af291b10556d56a33e7c2b75876aae9ae
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-08-28 12:20:18 +0100

    Fill vertical gaps

--- imageObjects/LineObject.py
+++ imageObjects/LineObject.py
@@ -29,6 +29,17 @@ class LineObject:
             sys.exit("LineObject expects an ImageObject")
 
     def find_vertical_lines(self, adjacent_pixels=None, fill_gaps_max_length=None):
+        """
+        This will look for vertical lines that meet the minimum width requirement
+
+        :param adjacent_pixels: If specified, this number of adjacent pixels will also be selected from each line found
+        :type adjacent_pixels: int | None
+
+        :param fill_gaps_max_length:  If specified, this will try to find and fill gaps between lines that might occur
+            on damaged or old images. The integer passed is the maximum gap you will permit, the min is taken from
+            min_length passed to the class object.
+        :type fill_gaps_max_length int | None
+        """
 
         # For each column of the image, find how many of the current column are not zero, grouping them by adjacency. If
         # the adjacent groups meet the minimum length requirement, draw them. Once all column are complete, isolate
@@ -40,9 +51,12 @@ class LineObject:
         # If we want to enlarge the lines a bit, we can do so via drawing them larger via draw_contours.
         if adjacent_pixels:
             [c.draw_contour(self.img, self._WHITE_BGR, adjacent_pixels) for c in self.img.find_contours("external")]
+
+        # If we want to try to bridge gaps between lines, this method will do so as long as the distance is less than
+        # the maximum length provided by the user via fill_gaps_max_length
+        if fill_gaps_max_length:
+            self._fill_vertical_gaps(fill_gaps_max_length)
         self.img.show()
-
-
 
     def find_horizontal_lines(self, adjacent_pixels=None, fill_gaps_max_length=None):
         """
@@ -55,7 +69,6 @@ class LineObject:
             on damaged or old images. The integer passed is the maximum gap you will permit, the min is taken from
             min_length passed to the class object.
         :type fill_gaps_max_length int | None
-
         """
         # For each row of the image, find how many of the current row are not zero, grouping them by adjacency. If the
         # adjacent groups meet the minimum length requirement, draw them. Once all rows are complete, isolate the lines
@@ -71,27 +84,6 @@ class LineObject:
         # the maximum length provided by the user via fill_gaps_max_length
         if fill_gaps_max_length:
             self._fill_horizontal_gaps(fill_gaps_max_length)
-
-    def _fill_horizontal_gaps(self, gap_max):
-        """
-        If a row has any pixels that are a line, we isolate this rows pixel values and break the row into lines. If
-        there is more than 1 segment, and the gap length between them is less than gap_max, then it is filled. If
-        block_out is set as a preference for failures, then if these lines where to small individual then they will be
-        removed.
-        """
-        row_pixel_values = np.sum(self.img.image == self._WHITE, axis=1)
-        for row_i, row_sum in enumerate(row_pixel_values):
-            if row_sum > 0:
-                row = self._isolate_row(row_i)
-
-                # If there are pixels in this row, isolate the gaps and if the gaps meet the criteria fill them
-                if (0 < self._isolate_gaps(row) < gap_max) and (row_sum > self._len_min):
-                    r_min = min(row)
-                    [self.img.change_pixel_colour(row_i, r_min + i, self._WHITE) for i in range(0, max(row) - r_min)]
-
-                # If we want to remove anything that fails the size check we can block it out
-                elif self._block_out and (row_sum < self._len_min):
-                    [self.img.change_pixel_colour(row_i, i, self._BLACK) for i in row]
 
     @staticmethod
     def _isolate_gaps(row):
@@ -131,3 +123,46 @@ class LineObject:
         For the horizontal draw lines our index i is our row index and our in line ii is the column index.
         """
         return [self.img.change_pixel_colour(i, ii, self._mark) for ii in x_indexes if len(x_indexes) > self._len_min]
+
+    def _fill_horizontal_gaps(self, gap_max):
+        """
+        If a row has any pixels that are a line, we isolate this rows pixel values and break the row into lines. If
+        there is more than 1 segment, and the gap length between them is less than gap_max, then it is filled. If
+        block_out is set as a preference for failures, then if these lines where to small individual then they will be
+        removed.
+        """
+        row_pixel_values = np.sum(self.img.image == self._WHITE, axis=1)
+        for row_i, row_sum in enumerate(row_pixel_values):
+            if row_sum > 0:
+                row = self._isolate_row(row_i)
+
+                # If there are pixels in this row, isolate the gaps and if the gaps meet the criteria fill them
+                if (0 < self._isolate_gaps(row) < gap_max) and (row_sum > self._len_min):
+                    r_min = min(row)
+                    [self.img.change_pixel_colour(row_i, r_min + i, self._WHITE) for i in range(0, max(row) - r_min)]
+
+                # If we want to remove anything that fails the size check we can block it out
+                elif self._block_out and (row_sum < self._len_min):
+                    [self.img.change_pixel_colour(row_i, i, self._BLACK) for i in row]
+
+    def _fill_vertical_gaps(self, gap_max):
+        """
+        If a column has any pixels that are a line, we isolate this columns pixel values and break the column into
+        lines. If there is more than 1 segment, and the gap length between them is less than gap_max, then it is filled.
+        If block_out is set as a preference for failures, then if these lines where to small individual then they will
+        be removed.
+        """
+        column_pixel_values = np.sum(self.img.image == self._WHITE, axis=0)
+        for column_i, column_sum in enumerate(column_pixel_values):
+            if column_sum > 0:
+                column = self._isolate_column(column_i)
+
+                # If there are pixels in this column, isolate the gaps and if the gaps meet the criteria fill them
+                if (0 < self._isolate_gaps(column) < gap_max) and (column_sum > self._len_min):
+                    c_min = min(column)
+                    [self.img.change_pixel_colour(c_min + i, column_i, self._WHITE)
+                     for i in range(0, max(column) - c_min)]
+
+                # If we want to remove anything that fails the size check we can block it out
+                elif self._block_out and (column_sum < self._len_min):
+                    [self.img.change_pixel_colour(i, column_i, self._BLACK) for i in column]
