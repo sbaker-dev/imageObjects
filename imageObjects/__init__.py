e255c14725933c074427ce0f8df8212a46b9f17b
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-12-22 15:16:26 +0000

    Fix incorrect point position parsing

--- imageObjects/__init__.py
+++ imageObjects/__init__.py
@@ -2,4 +2,5 @@
 from imageObjects.Support import create_text_box, create_blank
 from imageObjects.ContourObject import ContourObject
 from imageObjects.ImageObject import ImageObject
+from imageObjects.PolySupport import PolySupport
 from imageObjects.LineObject import LineObject
