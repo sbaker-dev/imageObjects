c183f301bd91cca9d7cc86247b9dd8b459c23efb
sbaker-dev <samuelbaker.researcher@gmail.com>
2022-04-12 16:08:20 +0100

    Validate the channels

--- UnitTests/UnitTest.py
+++ UnitTests/UnitTest.py
@@ -16,14 +16,13 @@ class MyTestCase(unittest.TestCase):
     def test_dimensions(self):
         """Test the image has not changed size by validating its known dimensions"""
         img = self.image
-
         self.assertEqual(img.width, 574)
         self.assertEqual(img.height, 380)
         self.assertEqual(img.pixel_total, 218120)
+        self.assertEqual(img.channels, 3)
 
     def test_invert(self):
         """Test that the inversion of the image matrix's first row BGR total is equal to the known dimension"""
-
         inverted = self.image.invert(True)
         self.assertEqual(np.sum(inverted[0], axis=0).tolist(), [17379, 23935, 26712])
 
