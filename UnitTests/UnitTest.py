2f4b80ce8f2448b6b8a14b3e920696f6d9ed47d4
sbaker-dev <samuelbaker.researcher@gmail.com>
2022-04-12 15:42:17 +0100

    Test Inversion

--- UnitTests/UnitTest.py
+++ UnitTests/UnitTest.py
@@ -2,23 +2,30 @@ from imageObjects.ImageObject import ImageObject
 from imageObjects.Support import load_image
 
 from pathlib import Path
+import numpy as np
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
 
     @property
-    def image_path(self):
+    def image(self):
         """Relative path to the image"""
-        return Path(Path(__file__).parent, 'WastWater.jpg')
+        return ImageObject(load_image(Path(Path(__file__).parent, 'WastWater.jpg')))
 
     def test_dimensions(self):
         """Test the image has not changed size by validating its known dimensions"""
-
-        img = ImageObject(load_image(self.image_path))
+        img = self.image
 
         self.assertEqual(img.width, 574)
         self.assertEqual(img.height, 380)
+        self.assertEqual(img.pixel_total, 218120)
+
+    def test_invert(self):
+        """Test that the inversion of the image matrix's first row BGR total is equal to the known dimension"""
+
+        inverted = self.image.invert(True)
+        self.assertEqual(np.sum(inverted[0], axis=0).tolist(), [17379, 23935, 26712])
 
 
 if __name__ == '__main__':
